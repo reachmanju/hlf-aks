f5d65b152d716f2c613c84405ad931754a16de68
reachmanju <42078329+reachmanju@users.noreply.github.com>
2019-02-05 23:16:19 -0600

    Update setup_blockchainNetwork.sh

--- setup_blockchainNetwork.sh
+++ setup_blockchainNetwork.sh
@@ -35,15 +35,7 @@ kubectl create -f ${KUBECONFIG_FOLDER}/copyArtifactsJob.yaml
 pod=$(kubectl get pods --selector=job-name=copyartifacts --output=jsonpath={.items..metadata.name})
 podSTATUS=$(kubectl get pods --selector=job-name=copyartifacts --output=jsonpath={.items..phase})
 
-while [ "${podSTATUS}" != "Running" ]; do
-    echo "Wating for container of copy artifact pod to run. Current status of ${pod} is ${podSTATUS}"
-    sleep 5;
-    if [ "${podSTATUS}" == "Error" ]; then
-        echo "There is an error in copyartifacts job. Please check logs."
-        exit 1
-    fi
-    podSTATUS=$(kubectl get pods --selector=job-name=copyartifacts --output=jsonpath={.items..phase})
-done
+sleep 30
 
 echo -e "${pod} is now ${podSTATUS}"
 echo -e "\nStarting to copy artifacts in persistent volume."
@@ -51,20 +43,10 @@ echo -e "\nStarting to copy artifacts in persistent volume."
 #fix for this script to work on icp and ICS
 kubectl cp ./artifacts $pod:/shared/
 
-echo "Waiting for 10 more seconds for copying artifacts to avoid any network delay"
-sleep 10
+sleep 120
+
 JOBSTATUS=$(kubectl get jobs |grep "copyartifacts" |awk '{print $2}')
-while [ "${JOBSTATUS}" != "1/1" ]; do
-    echo "Waiting for copyartifacts job to complete"
-    sleep 1;
-    PODSTATUS=$(kubectl get pods | grep "copyartifacts" | awk '{print $3}')
-        if [ "${PODSTATUS}" == "Error" ]; then
-            echo "There is an error in copyartifacts job. Please check logs."
-            exit 1
-        fi
-    JOBSTATUS=$(kubectl get jobs |grep "copyartifacts" |awk '{print $3}')
-done
-echo "Copy artifacts job completed"
+echo "${JOBSTATUS}"
 
 
 # Generate Network artifacts using configtx.yaml and crypto-config.yaml
@@ -73,7 +55,7 @@ echo -e "\nGenerating the required artifacts for Blockchain network"
 
 echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/cryptogen.yaml"
 kubectl create -f ${KUBECONFIG_FOLDER}/cryptogen.yaml
-sleep 35
+sleep 100
 
 # Genesis Block
 echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/configtxgen.yaml"
@@ -90,6 +72,7 @@ sleep 25
 echo -e "\nCreating new Deployment to create four peers in network"
 echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/peersDeployment.yaml"
 kubectl create -f ${KUBECONFIG_FOLDER}/peersDeployment.yaml
+sleep 25
 
 echo "Checking if all deployments are ready"
 
@@ -99,9 +82,6 @@ while [ "${NUMPENDING}" != "0" ]; do
     NUMPENDING=$(kubectl get deployments | grep blockchain | awk '{print $5}' | grep 0 | wc -l | awk '{print $1}')
     sleep 1
 done
-
-echo "Waiting for 15 seconds for peers and orderer to settle"
-sleep 15
 
 # Create services for all peers, ca, orderer
 echo -e "\nCreating Services for blockchain network"
@@ -117,7 +97,7 @@ echo -e "\nCreating channel transaction artifact and a channel"
 echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/create_channel.yaml"
 kubectl create -f ${KUBECONFIG_FOLDER}/create_channel.yaml
 
-sleep 35
+sleep 100
 
 
 # Join all peers on a channel
@@ -125,20 +105,20 @@ echo -e "\nCreating joinchannel job"
 echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/join_channel.yaml"
 kubectl create -f ${KUBECONFIG_FOLDER}/join_channel.yaml
 
-sleep 35
+sleep 100
 
 # Install chaincode on each peer
 echo -e "\nCreating installchaincode job"
 echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_install.yaml"
 kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_install.yaml
 
-sleep 45
+sleep 100
 
 # Instantiate chaincode on channel
 echo -e "\nCreating chaincodeinstantiate job"
 echo "Running: kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml"
 kubectl create -f ${KUBECONFIG_FOLDER}/chaincode_instantiate.yaml
 
-sleep 45
+sleep 120
 
 echo -e "\nNetwork Setup Completed !!"
